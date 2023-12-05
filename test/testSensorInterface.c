3445a79c8fea5742596ed1ab3c9b2f6e5b330dd4
ssggii <haan7525@gmail.com>
2023-12-05 21:50:22 +0900

    test: frontSensorInterface, leftSensorInterface, rightSensorInterface

--- test/testSensorInterface.c
+++ test/testSensorInterface.c
@@ -15,13 +15,19 @@
 
 */
 
+int d,f,l,r;
+
 void setUp(){ // before each test
 }
 
 void tearDown(){ // after each test
+    d = 0;
+    f = 0;
+    l = 0;
+    r = 0;
 }
 
-void detectDust_DustLevelLessThanOrEqulTo60_ReturnFalse(void)
+void detectDust_DustLevelLessThanOrEqulTo60_ReturnFalse(void) // dustLevel이 60 이하일 때 False(0) 반환하면 PASS
 {
     // Arrange
     int dustLevel1 = 40;
@@ -37,7 +43,7 @@ void detectDust_DustLevelLessThanOrEqulTo60_ReturnFalse(void)
 
 }
 
-void detectDust_DustLevelGreaterThan60_ReturnTrue(void)
+void detectDust_DustLevelGreaterThan60_ReturnTrue(void) // dustLevel이 60 초과일 때 True(1) 반환하면 PASS
 {
     // Arrange
     int dustLevel1 = 70;
@@ -53,10 +59,54 @@ void detectDust_DustLevelGreaterThan60_ReturnTrue(void)
     
 }
 
+void dustSensorInterface_Default_ReturnCorrectRangeInput(void) // 먼지 흡입량(input)이 0~100 범위에 있으면 PASS
+{
+    // Arrange, Act
+    int dustInput = dustSensorInterface();
+
+    // Assert
+    TEST_ASSERT_INT_WITHIN(50, 50, dustInput);
+
+}
+
+void frontSensorInterface_Default_ReturnCorrectRangeInput(void) // 전방 장애물 센서 input이 0 또는 1이면 PASS
+{
+    // Arrange, Act
+    f = frontSensorInterface();
+
+    // Assert
+    TEST_ASSERT_LESS_OR_EQUAL_UINT(1, f);
+
+}
+
+void leftSensorInterface_Default_ReturnCorrectRangeInput(void) // 좌측 장애물 센서 input이 0 또는 1이면 PASS
+{
+    // Arrange, Act
+    l = leftSensorInterface();
+
+    // Assert
+    TEST_ASSERT_LESS_OR_EQUAL_UINT(1, l);
+
+}
+
+void rightSensorInterface_Default_ReturnCorrectRangeInput(void) // 우측 장애물 센서 input이 0 또는 1이면 PASS
+{
+    // Arrange, Act
+    r = rightSensorInterface();
+
+    // Assert
+    TEST_ASSERT_LESS_OR_EQUAL_UINT(1, r);
+
+}
+
 int main(void)
 {
 	UNITY_BEGIN();
 	RUN_TEST(detectDust_DustLevelLessThanOrEqulTo60_ReturnFalse);
 	RUN_TEST(detectDust_DustLevelGreaterThan60_ReturnTrue);
+    RUN_TEST(dustSensorInterface_Default_ReturnCorrectRangeInput);
+    RUN_TEST(frontSensorInterface_Default_ReturnCorrectRangeInput);
+    RUN_TEST(leftSensorInterface_Default_ReturnCorrectRangeInput);
+    RUN_TEST(rightSensorInterface_Default_ReturnCorrectRangeInput);
 	return UNITY_END();
 }
