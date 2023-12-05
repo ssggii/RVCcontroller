e224b71fb74b2c1a8a9a09eaf2b0f5b7d59ce4f7
ssggii <haan7525@gmail.com>
2023-12-05 13:52:53 +0900

    fix: make header file for main.c

--- main.c
+++ main.c
@@ -1,24 +1,5 @@
 #include <stdio.h>
-
-int detectDust();
-void detectObstacleDirection(int obstacleArray[]);
-int dustSensorInterface();
-int frontSensorInterface();
-int leftSensorInterface();
-int rightSensorInterface();
-int performActionByMode(int d,int f, int l, int r,int mode);
-void performActionForward(int f, int l, int r, int mode);
-void performActionBackward(int f, int l, int r, int mode);
-void turnFirstActionFromForward(int f, int l, int r, int mode);
-void turnFirstActionFromBackward(int f, int l, int r, int mode);
-void turnSecondAction();
-void moveForward(int command);
-void cleanerSwitch(int command);
-void powerUpCleaner();
-void turn(int f, int l, int r, int mode);
-void turnLeft();
-void turnRight();
-void moveBackward(int command);
+#include "main.h"
 
 int main(void){
     int d,f,l,r;
