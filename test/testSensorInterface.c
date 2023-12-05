7ddfb1332f7d09fcf0a485404225a48d2696005d
ssggii <haan7525@gmail.com>
2023-12-05 16:32:36 +0900

    fix: rename testSensorInterface.c

---
+++ test/testSensorInterface.c
@@ -0,0 +1,62 @@
+#include "../unity/unity.h"
+#include "../main.h"
+
+/*
+    Unit testing for sensor intefaces implemented by:
+
+    int detectDust();
+    int dustSensorInterface();
+    int frontSensorInterface();
+    int leftSensorInterface();
+    int rightSensorInterface();
+    void detectObstacleDirection(int obstacleArray[]);
+
+}
+
+*/
+
+void setUp(){ // before each test
+}
+
+void tearDown(){ // after each test
+}
+
+void detectDust_DustLevelLessThanOrEqulTo60_ReturnFalse(void)
+{
+    // Arrange
+    int dustLevel1 = 40;
+    int dustLevel2 = 60;
+    
+    // Act
+    int result1 = detectDust(dustLevel1);
+    int result2 = detectDust(dustLevel2);
+
+    // Assert
+    TEST_ASSERT_EQUAL_INT(0, result1);
+    TEST_ASSERT_EQUAL_INT(0, result2);
+
+}
+
+void detectDust_DustLevelGreaterThan60_ReturnTrue(void)
+{
+    // Arrange
+    int dustLevel1 = 70;
+    int dustLevel2 = 100;
+    
+    // Act
+    int result1 = detectDust(dustLevel1);
+    int result2 = detectDust(dustLevel2);
+
+    // Assert
+    TEST_ASSERT_EQUAL_INT(1, result1);
+    TEST_ASSERT_EQUAL_INT(1, result2);
+    
+}
+
+int main(void)
+{
+	UNITY_BEGIN();
+	RUN_TEST(detectDust_DustLevelLessThanOrEqulTo60_ReturnFalse);
+	RUN_TEST(detectDust_DustLevelGreaterThan60_ReturnTrue);
+	return UNITY_END();
+}
