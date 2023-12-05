833919873594c987b6d36c19371ce789f0ba116d
ssggii <haan7525@gmail.com>
2023-12-05 15:14:53 +0900

    feat: seperate functions and main for testing

---
+++ functions.c
@@ -0,0 +1,151 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+
+int detectDust(int dustInput){ 
+   int dustLevel = dustInput;
+   int isDustDetected = 0;
+   if(dustLevel > 60){
+        isDustDetected = 1;
+   }
+   return isDustDetected;
+}
+
+void detectObstacleDirection(int obstacleArray[]) {
+    obstacleArray[0] = frontSensorInterface();  // F input
+    obstacleArray[1] = leftSensorInterface();   // L input
+    obstacleArray[2] = rightSensorInterface();  // R input
+}
+
+int dustSensorInterface(){ // Detect dust level from sensor
+    int dustLevel;
+    srand(time(NULL));
+    dustLevel = rand() % 100 + 1; // range from 1 to 100
+    return dustLevel;
+}
+
+int frontSensorInterface(){ // Detect F input
+    int isObstacleDetectedOnFront;
+    printf("Please enter the F input(1 for true, 0 for false): ");
+    scanf("%d", &isObstacleDetectedOnFront);
+    while (getchar() != '\n');
+    return isObstacleDetectedOnFront;
+}
+
+int leftSensorInterface(){ // Detect L input
+    int isObstacleDetectedOnLeft;
+    printf("Please enter the L input(1 for true, 0 for false): ");
+    scanf("%d", &isObstacleDetectedOnLeft);
+    while (getchar() != '\n');
+    return isObstacleDetectedOnLeft;
+}
+
+int rightSensorInterface(){ // Detect R input
+    int isObstacleDetectedOnRight;
+    printf("Please enter the R input(1 for true, 0 for false): ");
+    scanf("%d", &isObstacleDetectedOnRight);
+    while (getchar() != '\n');
+    return isObstacleDetectedOnRight;
+}
+
+int performActionByMode(int d, int f, int l, int r, int mode){
+    if (mode == 1 && f == 0){
+        if (d == 1){
+            powerUpCleaner();
+        }else{
+            printf("\n");
+        }
+        return 1;
+    }else if (mode == 1){
+        performActionForward(f, l, r, mode);
+        if (f == 1 && l == 1 && r == 1)
+            return 0;
+    }else if (mode == 0){
+        performActionBackward(f, l, r, mode);
+        return 1;
+    }
+}
+
+void performActionForward(int f, int l, int r, int mode){
+    turnFirstActionFromForward(f, l, r, mode);
+    if (f == 1 && l == 1 && r == 1)
+        return;
+    turnSecondAction();
+}
+
+void performActionBackward(int f, int l, int r, int mode){
+    turnFirstActionFromBackward(f, l, r, mode);
+    turnSecondAction();
+}
+
+
+void turnFirstActionFromForward(int f, int l, int r, int mode){
+    moveForward(0);
+    cleanerSwitch(0);
+    turn(f, l, r, mode);
+}
+
+void turnFirstActionFromBackward(int f, int l, int r, int mode){
+    turn(f, l, r, mode);
+}
+
+void turnSecondAction(){
+    moveForward(1);
+    cleanerSwitch(1);
+}
+
+void moveForward(int command){
+    if (command == 1){ //enable
+        printf("enable move forward\n");
+    }else if (command == 0){ //disable
+        printf("disable move forward\n");
+    }
+}
+
+void cleanerSwitch(int command){
+    if (command == 1){ // turn on
+        printf("turn on cleaner\n");
+    }else if (command == 0){ // turn off
+        printf("turn off cleaner\n");
+    }
+}
+
+void powerUpCleaner(){
+    printf("power up cleaner\n");
+}
+
+void turn(int f, int l, int r, int mode){
+    if (mode == 1){
+        if (f && !l){
+            turnLeft();
+        }else if (f && l && !r){
+            turnRight();
+        }else if (f && l && r){
+            moveBackward(1);
+        }
+    }else if (mode == 0){
+        moveBackward(0);
+        if (!l){
+            turnLeft();
+        }else if (l && !r){
+            turnRight();
+        }
+    }
+}
+
+void turnLeft(){
+    printf("trigger turn left\n");
+}
+
+void turnRight(){
+    printf("trigger turn right\n");
+}
+
+void moveBackward(int command){
+    if (command == 1){ //enable
+        printf("enable move backward\n");
+    }else if (command == 0){ //disable
+        printf("disable move backward\n");
+    }
+}
